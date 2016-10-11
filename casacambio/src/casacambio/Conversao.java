297571b9a255ff9a384172758bd6ce6c4bb618fe
c165921 <c165921@LI050>
2016-10-11 11:01:53 -0300

    Caroline Resende Silveira - Funções com argumentos de tamanho variavel. + JAVADOC

--- casacambio/src/casacambio/Conversao.java
+++ casacambio/src/casacambio/Conversao.java
@@ -38,4 +38,44 @@ public class Conversao {
   
     }
     
+        /* Operacoes matematicas ... Caroline Resende */
+    
+    /* SOBRECARGA de METODOS */
+    public float mediaCompra (float x1, float x2) {
+        return (x1+x2)/2;
+    }
+    
+    public float mediaVenda (float x1, float x2) {
+        return (x1+x2)/2;
+    }
+    
+    public float mediaCompra (float x1, float x2, float x3) {
+        return (x1+x2+x3)/3;
+    }
+    
+    public float mediaVenda (float x1, float x2, float x3) {
+        return (x1+x2+x3)/3;
+    }
+    
+    /* FUNCAO COM LISTA DE ARGUMENTOS DE COMPRIMENTO VARIAVEL - MEDIA */
+    public float mediaVenda(float... x){
+        float media=0;
+        
+        for(float valor: x){
+            media +=valor;
+        }
+        
+        return media/x.length;
+    }
+
+    public float mediaCompra(float... x){
+        float media=0;
+        
+        for(float valor: x){
+            media +=valor;
+        }
+        
+        return media/x.length;
+    }
+    
 }
